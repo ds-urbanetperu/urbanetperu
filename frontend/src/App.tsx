a29a3fa75c6ea7fa9b783ce0ac92a0014ab31aab
Faalvial <farid23012006@gmail.com>
2026-04-28 17:00:58 -0500

    feat(URB-29): implementar registro y recuperación de contraseña

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,25 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ForgotPassword from "./pages/ForgotPassword";
+import ForgotPasswordSent from "./pages/ForgotPasswordSent";
+import ResetPassword from "./pages/ResetPassword";
+import ResetPasswordSuccess from "./pages/ResetPasswordSuccess";
 
 function App() {
-  return <Login />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registro" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/forgot-password/sent" element={<ForgotPasswordSent />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/reset-password/success" element={<ResetPasswordSuccess />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
